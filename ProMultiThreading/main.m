59a023e298a1471fbdb695c23ad19292d06107d8
marjinn <marjinn@live.com>
2014-05-21 00:32:00 +0530

    Initial Commit

---
+++ ProMultiThreading/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ProMultiThreading
+//
+//  Created by mar Jinn on 5/21/14.
+//  Copyright (c) 2014 mar Jinn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
